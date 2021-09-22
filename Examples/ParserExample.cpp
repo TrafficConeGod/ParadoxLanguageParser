a4b1007d522e74b2260de4d97bfb5d8b7395ed44
Traffic <trafficconegod@gmail.com>
2021-09-22 17:08:59 -0400

    add basic parsing

--- Examples/ParserExample.cpp
+++ Examples/ParserExample.cpp
@@ -8,9 +8,12 @@ int main() {
     stream << std::ifstream("ParserExample.txt").rdbuf();
     ParadoxLanguage::Object object(stream.str());
 
-    // for (auto key : object.Keys()) {
-    //     std::cout << key << "\n";
-    // }
+    for (auto key : object.Keys()) {
+        std::cout << key << ":\n";
+        for (auto value : object.AllAt(key)) {
+            std::cout << "\t" << std::any_cast<std::string>(value) << "\n";
+        }
+    }
 
     return 0;
 }
